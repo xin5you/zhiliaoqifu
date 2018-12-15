da9dee6c7a8730c89eb422e6260fcf254058e0bf
zhu_qiuyou <zhu_qiuyou@ebeijia.com>
2018-12-15 19:48:22 +0800

    电商API应用测试环境发布

---
+++ 00-code/zl-maindir/zl-web-shop/src/main/java/com/ebeijia/zl/ShopApp.java
@@ -0,0 +1,21 @@
+package com.ebeijia.zl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.WebApplicationInitializer;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class ShopApp extends SpringBootServletInitializer implements WebApplicationInitializer {
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ShopApp.class);
+	}
+	
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApp.class,args);
+    }
+}
