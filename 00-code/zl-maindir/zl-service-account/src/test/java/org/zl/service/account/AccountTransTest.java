b517ced553091c4fdfb167b2f042428e54bf7a26
jiren <sun.ops@hotmail.com>
2018-12-23 22:16:37 +0800

    D

--- 00-code/zl-maindir/zl-service-account/src/test/java/org/zl/service/account/AccountTransTest.java
+++ 00-code/zl-maindir/zl-service-account/src/test/java/org/zl/service/account/AccountTransTest.java
@@ -1,26 +1,18 @@
 package org.zl.service.account;
 
 
-import java.math.BigDecimal;
-
+import com.ebeijia.zl.AccountApp;
+import com.ebeijia.zl.common.utils.IdUtil;
+import com.ebeijia.zl.common.utils.enums.*;
+import com.ebeijia.zl.facade.account.req.AccountRechargeReqVo;
+import com.ebeijia.zl.facade.account.service.AccountTransactionFacade;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.alibaba.fastjson.JSONArray;
-import com.ebeijia.zl.AccountApp;
-import com.ebeijia.zl.common.utils.IdUtil;
-import com.ebeijia.zl.common.utils.domain.BaseResult;
-import com.ebeijia.zl.common.utils.enums.SpecAccountTypeEnum;
-import com.ebeijia.zl.common.utils.enums.TransChnl;
-import com.ebeijia.zl.common.utils.enums.TransCode;
-import com.ebeijia.zl.common.utils.enums.UserChnlCode;
-import com.ebeijia.zl.common.utils.enums.UserType;
-import com.ebeijia.zl.facade.account.req.AccountRechargeReqVo;
-import com.ebeijia.zl.facade.account.req.AccountTransferReqVo;
-import com.ebeijia.zl.facade.account.service.AccountTransactionFacade;
+import java.math.BigDecimal;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = AccountApp.class)//这里的Application是springboot的启动类名
