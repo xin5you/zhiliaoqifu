79d72a57af73f4d4ca52c79742520e0bd4cb3045
jiren <sun.ops@hotmail.com>
2019-01-17 10:06:12 +0800

    Fix bug,直充接口处理

--- 00-code/zl-maindir/zl-basics-coupon/src/main/java/com/ebeijia/zl/coupon/dao/mapper/TbCouponProductMapper.java
+++ 00-code/zl-maindir/zl-basics-coupon/src/main/java/com/ebeijia/zl/coupon/dao/mapper/TbCouponProductMapper.java
@@ -1,13 +1,12 @@
 package com.ebeijia.zl.coupon.dao.mapper;
 
-	import com.ebeijia.zl.coupon.dao.domain.TbCouponProduct;
-	import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-	import org.apache.ibatis.annotations.Mapper;
+import com.ebeijia.zl.coupon.dao.domain.TbCouponProduct;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
-    import java.util.List;
+import java.util.List;
 
 /**
- *
  * 卡密产品为卡密的大分类 Mapper 接口
  *
  * @User J
@@ -16,10 +15,11 @@ package com.ebeijia.zl.coupon.dao.mapper;
 @Mapper
 public interface TbCouponProductMapper extends BaseMapper<TbCouponProduct> {
 
-	/**
-	 * 根据条件查询商品规格信息
-	 * @param couponProduct
-	 * @return
-	 */
+    /**
+     * 根据条件查询商品规格信息
+     *
+     * @param couponProduct
+     * @return
+     */
     List<TbCouponProduct> getCouponList(TbCouponProduct couponProduct);
 }
